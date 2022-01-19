d6e984fd0f048391d848b86a7e9433c766c94e4d
aa <aa>
2022-01-19 22:30:33 +0800

    fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,37 +19,4 @@ export const cleanObject = (object: any) => {
   return result
 }
 
-export const useMount = (callBack: () => void) => {
-  useEffect(() => {
-    callBack()
-  }, [])
-}
-
-export const useDebounce = <V> (value: V, delay?: number) => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
-
-  useEffect(() => {
-    // 每次在value变化以后，设置一个定时器
-    const timeout = setTimeout(() => setDebouncedValue(value), delay)
-    // 每次在上一个useEffect处理完以后再运行
-    return () => clearTimeout(timeout);
-  }, [value, delay]);
-
-  return debouncedValue;
-}
-
-export const useArray = <T> (initialArray: T[]) => {
-  const [value, setValue] = useState(initialArray)
-  return {
-    value,
-    setValue,
-    add: (item: T) => setValue([...value, item]),
-    clear: () => setValue([]),
-    removeIndex: (index: number) => {
-      const copy = [...value]
-      copy.splice(index, 1)
-      setValue(copy)
-    }
-  }
-}
 
